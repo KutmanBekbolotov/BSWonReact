9570251bf62b1855275b0db87d7028eb737ae31b
KutmanBekbolotov <kutmanbekbolotov2003@gmail.com>
2024-06-08 23:56:46 +0600

    Added Overlay and rendering on new site

--- src/App.js
+++ src/App.js
@@ -3,18 +3,6 @@ import { Helmet } from 'react-helmet';
 import './App.css';
 
 function App() {
-  // function moreInfo(){
-  //   document.addEventListener("DOMContentLoaded", function(){
-  //     const newOverlay = document.getElementById("More");
-  //     const Overlay = [
-  //       {id: 1}
-  //     ];
-  //     if (Overlay){
-  //       window.location.href = "moreinfo.js";
-  //     }
-  //   })
-  // };
-
   function scrollToTop() {
     window.scrollTo({
       top: 0,
@@ -36,12 +24,13 @@ function App() {
     
     return () => {
       window.removeEventListener('scroll', handleScroll);
-    };
-  },[]);
-  
+      };
+      },[]);
+      
 
-  return (
-    <div className="App">
+
+      return (
+        <div className="App">
       <Helmet>
         <link href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css" rel="stylesheet" />
       </Helmet>
@@ -51,7 +40,7 @@ function App() {
           <h1>Bulgass IT-academy</h1>
           <h3>Exploring computer science</h3>
           <div className="contact-info">  
-          <p> Contact number: 220-04-19-43 &#9990; </p>
+          <p> Contact number: 220-041-943 &#9990; </p>
           <p>Contact whatsapp-number: 550-092-903<svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" className="bi bi-whatsapp" viewBox="0 -5 10 20">
   <path d="M13.601 2.326A7.854 7.854 0 0 0 7.994 0C3.627 0 .068 3.558.064 7.926c0 1.399.366 2.76 1.057 3.965L0 16l4.204-1.102a7.933 7.933 0 0 0 3.79.965h.004c4.368 0 7.926-3.558 7.93-7.93A7.898 7.898 0 0 0 13.6 2.326zM7.994 14.521a6.573 6.573 0 0 1-3.356-.92l-.24-.144-2.494.654.666-2.433-.156-.251a6.56 6.56 0 0 1-1.007-3.505c0-3.626 2.957-6.584 6.591-6.584a6.56 6.56 0 0 1 4.66 1.931 6.557 6.557 0 0 1 1.928 4.66c-.004 3.639-2.961 6.592-6.592 6.592zm3.615-4.934c-.197-.099-1.17-.578-1.353-.646-.182-.065-.315-.099-.445.099-.133.197-.513.646-.627.775-.114.133-.232.148-.43.05-.197-.1-.836-.308-1.592-.985-.59-.525-.985-1.175-1.103-1.372-.114-.198-.011-.304.088-.403.087-.088.197-.232.296-.346.1-.114.133-.198.198-.33.065-.134.034-.248-.015-.347-.05-.099-.445-1.076-.612-1.47-.16-.389-.323-.335-.445-.34-.114-.007-.247-.007-.38-.007a.729.729 0 0 0-.529.247c-.182.198-.691.677-.691 1.654 0 .977.71 1.916.81 2.049.098.133 1.394 2.132 3.383 2.992.47.205.84.326 1.129.418.475.152.904.129 1.246.08.38-.058 1.171-.48 1.338-.943.164-.464.164-.86.114-.943-.049-.084-.182-.133-.38-.232z"/>
 </svg></p>
@@ -82,6 +71,18 @@ function App() {
           <div className="info">
             <h1>This is Bulgass</h1>
             <p>We will explore the best ways of IT industry.</p>
+            <div>
+              <div className='Overlay'>
+                      <label for="css-modal-checkbox" class="css-modal-checkbox">Название кнопки</label>
+                  </div>
+                  <input type="checkbox" id="css-modal-checkbox" />    
+                  <div class="cmc">
+                      <div class="cmt">
+                          <p>Текст в модальном окне</p>
+                      </div>
+                      <label for="css-modal-checkbox" class="css-modal-close"></label>
+              </div>
+          </div>
           </div>
         </div>
       </section>
@@ -89,7 +90,7 @@ function App() {
       <section id="features">
         <div className="header">
           <h1>Courses</h1>
-          <button className='glow-on-hover' onClick={()=> window.location.href = "moreinfo.js"}>See More</button>
+          <button className='glow-on-hover' onClick={()=> window.location.href = "https://bulgass-courses.netlify.app/"}>See More</button>
         </div>
         <div className="feature-cards">
           <div className="card">
